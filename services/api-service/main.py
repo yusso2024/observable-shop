b5cd97043bdb51d01ccd6163527ec96e7c9b17ca
yusso2024 <yusso2024@users.noreply.github.com>
2026-04-29 16:49:22 +0000

    Lab 2: Redis

--- services/api-service/main.py
+++ services/api-service/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
 app = FastAPI()
+import redis, json, time, os
+
+app = FastAPI()
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "redis"),
+    port=6379,
+    decode_responses=True
+)
 
 @app.get("/items")
 def get_items():
-    return {"items": ["apple", "banana", "carrot"]}
+    cached = r.get("items")
+    if cached:
+        return {"source": "cache", "items": json.loads(cached)}
+
+    time.sleep(1)
+    items = ["apple", "banana", "carrot"]
+
+    r.set("items", json.dumps(items), ex=30)
+
+    return {"source": "db", "items": items}
